5968ff6296291e7bfa5a3fe185187642a1839bfa
Sherry <z5692569@ad.unsw.edu.au>
2026-05-19 14:54:49 +1000

    created constants file and added users + data objects

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+// Stores the constants used 
+
+export let users = {
+    age: 0,
+    name: '',
+    photo: '',
+    bio: '',
+    gym: '',
+    location: '',
+    goals: '',
+    gender: '',
+    gym_experience: 0  
+}
+
+export let data = {
+    users: [],
+    idHistory: {
+        userIdTracker: 0
+    }
+}
